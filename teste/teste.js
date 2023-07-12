f0a2a4c4bc8903922efcd638e09796fe147fdee6
Gabriel Richard <richard.gabriel150@yahoo.com.br>
2023-07-11 23:32:48 -0300

    portfolio

---
+++ teste/teste.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    var burger = document.querySelector('.burger');
+  
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('toggle');
+  }
+  
