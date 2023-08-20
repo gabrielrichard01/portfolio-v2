c75c445d0fde9c756964b839cc102cea9a5292d8
Gabriel Richard <richard.gabriel150@yahoo.com.br>
2023-08-20 15:54:08 -0300

    pastas, novas tecnologias e novos projetos
    atualização

--- teste/teste.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var navLinks = document.querySelector('.nav-links');
-    var burger = document.querySelector('.burger');
-  
-    navLinks.classList.toggle('active');
-    burger.classList.toggle('toggle');
-  }
-  
